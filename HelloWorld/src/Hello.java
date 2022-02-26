1d1fdcb9bc0fdcb0cd411156abd0a7b97de079b5
Andrew Derbak <andrewderbak@gmail.com>
2022-02-26 14:34:39 -0600

    Updated Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,18 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Andrew!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal); // use sout as a shortcut to Standard Output
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+
     }
 }

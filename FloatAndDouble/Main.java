48d391e361d1f8b716565786c92463ad6eba2e9a
Andrew Derbak <andrewderbak@gmail.com>
2022-03-08 16:43:42 -0600

    Initial Commit

---
+++ FloatAndDouble/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Values = " + myMinFloatValue);
+        System.out.println("Float Maximum Values = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Values = " + myMinDoubleValue);
+        System.out.println("Double Maximum Values = " + myMaxDoubleValue);
+
+
+    }
+}

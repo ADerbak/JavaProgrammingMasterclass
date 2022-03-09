7adb99b81c2afc5835c953ec3a96fd41ab768a61
Andrew Derbak <andrewderbak@gmail.com>
2022-03-08 18:06:32 -0600

    FloatAndDouble Initial Commit

--- FloatAndDouble_v2/src/com/company/Main.java
+++ FloatAndDouble_v2/src/com/company/Main.java
@@ -15,5 +15,15 @@ public class Main {
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double Minimum Value :" + myMinDoubleValue);
         System.out.println("Double Maximum Value :" + myMaxDoubleValue);
+
+        int myIntValue = 5/3;
+        float myFloatValue = 5f/3f;
+        double myDoubleValue = 5d/3d;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= "+ myDoubleValue);
+
+
+
     }
 }

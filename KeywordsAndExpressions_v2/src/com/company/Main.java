3ca7f5cf55ed9a82a969eefb63c38169e77db113
Andrew <andrewderbak@gmail.com>
2022-04-02 16:28:23 -0500

    Updates calculateScore method to take parameters

--- KeywordsAndExpressions_v2/src/com/company/Main.java
+++ KeywordsAndExpressions_v2/src/com/company/Main.java
@@ -11,27 +11,28 @@ public class Main {
         int bonus = 100;
 
         // New method put in place
-        calculateScore();
+        calculateScore(true, 800, 5, 100);
 
         // Moved to a new method
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 10000, 8, 200);
+//        if (gameOver) {
+//            int finalScore = score + (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
     }
 
-    public static void calculateScore() {
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         // public and static are keywords
 
         // anything in the braces is part of the calculateScore() method
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+//        boolean gameOver = true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
 
         if (gameOver) {
             // variables can be created in code block, but cannot be accessed outside of code block

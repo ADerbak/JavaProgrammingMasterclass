c35d9d86a84a17067aa1d5196d04f05227e47f05
Andrew <andrewderbak@gmail.com>
2022-04-02 16:30:26 -0500

    Updates calculateScore method to take parameters. Removes variables.

--- KeywordsAndExpressions_v2/src/com/company/Main.java
+++ KeywordsAndExpressions_v2/src/com/company/Main.java
@@ -5,18 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Code Blocks and the IF-THEN-ELSE control statements
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+//        boolean gameOver = true;
+//        int score = 800;
+//        int levelCompleted = 5;
+//        int bonus = 100;
 
         // New method put in place
         calculateScore(true, 800, 5, 100);
 
         // Moved to a new method
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+//        score = 10000;
+//        levelCompleted = 8;
+//        bonus = 200;
 
         calculateScore(true, 10000, 8, 200);
 //        if (gameOver) {

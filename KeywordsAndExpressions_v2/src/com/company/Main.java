c0d253cc4f86887bf641575f06403f45a0ee6a8b
Andrew <andrewderbak@gmail.com>
2022-04-02 16:36:41 -0500

    Returns a value for finalScore.

--- KeywordsAndExpressions_v2/src/com/company/Main.java
+++ KeywordsAndExpressions_v2/src/com/company/Main.java
@@ -8,14 +8,17 @@ public class Main {
         calculateScore(true, 10000, 8, 200);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         // public and static are keywords
+        // "void" means don't return anything back. If we change this, it can return a value
         // anything in the braces is part of the calculateScore() method
         if (gameOver) {
             // variables can be created in code block, but cannot be accessed outside of code block
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return finalScore;
         }
+        return -1; // return -1 if gameOver is not true.
     }
 }

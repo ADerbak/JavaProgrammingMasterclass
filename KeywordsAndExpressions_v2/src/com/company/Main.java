78dcc21b65aa05b060e1ec490f9ef3568629d4e9
Andrew <andrewderbak@gmail.com>
2022-04-02 20:36:29 -0500

    Creates two new methods as a challenge to create a high score message.

--- KeywordsAndExpressions_v2/src/com/company/Main.java
+++ KeywordsAndExpressions_v2/src/com/company/Main.java
@@ -4,8 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         // New method put in place
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+
+        int highscore = calculateScore(true, 800, 5, 100);
+        System.out.println("Your final score was " + highscore);
+
+        highscore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highscore);
+
+        String player1 = displayHighScore("Andrew", calculateHighScore(1500));
+        System.out.println(player1);
+        String player2 = displayHighScore("Becca", calculateHighScore(900));
+        System.out.println(player2);
+        String player3 = displayHighScore("Cathy", calculateHighScore(400));
+        System.out.println(player3);
+        String player4 = displayHighScore("Doug", calculateHighScore(50));
+        System.out.println(player4);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -16,9 +29,31 @@ public class Main {
             // variables can be created in code block, but cannot be accessed outside of code block
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1; // return -1 if gameOver is not true.
+    }
+
+    public static String displayHighScore(String playerName, int position) {
+
+        String message =  playerName +
+                " managed to get into position " +
+                position +
+                " on the high score table";
+        return message;
+    }
+
+    public static int calculateHighScore(int score) {
+        int position = 4;
+        if (score > 1000){
+            position = 1;
+        } else if (score >= 500) {
+            position = 2;
+        } else if (score >= 100) {
+            position = 3;
+        } else {
+            position = 4;
+        }
+        return position;
     }
 }

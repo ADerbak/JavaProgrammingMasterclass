5d03ea336e67fb5d59a381dc3ae8c37ed8e541ed
Andrew <andrewderbak@gmail.com>
2022-04-02 20:42:06 -0500

    Simplifies calculateHighScore method.

--- KeywordsAndExpressions_v2/src/com/company/Main.java
+++ KeywordsAndExpressions_v2/src/com/company/Main.java
@@ -40,16 +40,14 @@ public class Main {
     }
 
     public static int calculateHighScore(int score) {
-        int position = 4;
         if (score > 1000){
-            position = 1;
+            return 1;
         } else if (score >= 500) {
-            position = 2;
+            return 2;
         } else if (score >= 100) {
-            position = 3;
+            return 3;
         } else {
-            position = 4;
+            return 4;
         }
-        return position;
     }
 }

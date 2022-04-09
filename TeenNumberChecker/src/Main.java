1677e66adcc98182782f10d629042fcaeca76c19
Andrew <andrewderbak@gmail.com>
2022-04-08 19:28:09 -0500

    Finalizes TeenNumberChecker

--- TeenNumberChecker/src/Main.java
+++ TeenNumberChecker/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        boolean result = TeenNumberChecker.hasTeen(22, 23, 34);
+        System.out.println(result);
+    }
 }
--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,16 +1,8 @@
 public class TeenNumberChecker {
     public static boolean hasTeen(int num1, int num2, int num3) {
-        if (isTeen(num1) || isTeen(num2) || isTeen(num3)) {
-            return true;
-        }
-        return false;
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
     }
-
     public static boolean isTeen(int number) {
-        if (number >= 13 && number <=19){
-            return true;
-        }
-        return false;
-
+        return number >= 13 && number <= 19;
     }
 }

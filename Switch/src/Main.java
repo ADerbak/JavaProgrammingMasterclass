18506fef683796d34c7cbc8e9389ab21552d4f09
Andrew <andrewderbak@gmail.com>
2022-04-18 15:44:32 -0500

    Updates with more switch cases and comments.

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -10,19 +10,26 @@ public class Main {
 //            System.out.println("Value was not 1 or 2");
 //        }
 
-        int switchValue = 1;
+        int switchValue = 3;
 
-        switch (switchValue){
+        switch (switchValue){ // only tests for the value
             case 1:
                 System.out.println("Value was 1");
-                break;
+                break;  // break is put in to jump out of switch
+            // if no break is specified, the output will continue to the next case's output.
 
             case 2:
                 System.out.println("Value was 2");
                 break;
 
+            case 3: case 4: case 5: //tests for multiple cases with one output
+                System.out.println("Value was a 3, 4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
             default:
                 System.out.println("Value was not 1 or 2");
+                break; // This isn't necessary for the code, but it is good for readability
         }
 
         // Break comes to here

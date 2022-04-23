bac9a9b2753f0f8261c2dbc4fec2974d390885bc
Andrew <andrewderbak@gmail.com>
2022-04-23 07:05:24 -0500

    Creates multiple inputs

--- ReadingUserInput/src/Main.java
+++ ReadingUserInput/src/Main.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); //need to do next line to allow for multiple inputs
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
+        int age = 2022 - yearOfBirth;
 
-        System.out.println("Your name is " + name);
+        System.out.println("Your name is " + name +
+                ", and you are " + age + " years old.");
 
         scanner.close();
     }

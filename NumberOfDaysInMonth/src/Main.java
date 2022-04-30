f893826b2a8673a10a2589ade85b0c56cf71137a
Andrew <andrewderbak@gmail.com>
2022-04-29 21:34:17 -0500

    Finalizes NumberOfDaysInMonth project.

--- NumberOfDaysInMonth/src/Main.java
+++ NumberOfDaysInMonth/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        int result = NumberOfDaysInMonth.getDaysInMonth(-1, 2018);
+        System.out.println("The number of days: " + result);
+    }
 }

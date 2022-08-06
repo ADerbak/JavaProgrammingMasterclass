2599306fae9452ebd75531a81b7a635a667d35df
Andrew <andrewderbak@gmail.com>
2022-08-06 08:15:28 -0500

    Completes two challenges for while loops.

--- DoWhile/src/DoWhile.java
+++ DoWhile/src/DoWhile.java
@@ -24,12 +24,53 @@ public class DoWhile {
             }
         } while (count != 6);
 
-
+        // Challenge!
         // Create a method called isEvenNumber that tkaes a parameter of type int
         // ITs purpose is to determine if the argument passed to the mehtod is an even number or not.
         // return true if an even number, otherwise return false;
 
         System.out.println("3 is an even number: " + isEvenNumber(3));
+
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number <= finishNumber){
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+        }
+
+
+        // Challenge!
+        // Modify the while code above
+        // Make it also record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end, display the total number of even numbers
+
+        number = 4;
+        finishNumber = 20;
+        int counter = 0;
+        int total = 0;
+        do {
+
+            number++;
+            if(!isEvenNumber(number)){
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+            counter++;
+            if (counter <=5){
+                total += number;
+            } else {
+                break;
+            }
+        } while (number <= finishNumber);
+
+        System.out.println("Total sum of evens: " + total);
     }
 
     public static boolean isEvenNumber(int num){

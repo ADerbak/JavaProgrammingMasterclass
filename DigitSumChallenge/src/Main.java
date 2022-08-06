43dbacabf78a65fed0b5bd2a0047e62aec02bf1f
Andrew <andrewderbak@gmail.com>
2022-08-06 08:45:57 -0500

    Simplifies code!

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(155125));
     }
 
     public static int sumDigits(int number){
@@ -10,18 +10,11 @@ public class Main {
         if (number < 10){
             return -1;
         } else {
-            while(number != 0) {
-                int value = 0;
-                for (int i = 1000; i >= 1; i /= 10) {
-                    value = number / i;
-                    total += value;
-                    number -= value * i;
-
-
+            while(number > 0) {
+                total += number % 10;
+                number /= 10;
                 }
             }
-
-        }
         return total;
     }
 }

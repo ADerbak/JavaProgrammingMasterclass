d9a7779aaec849bdeef8b627e5da4671d5f51a82
Andrew <andrewderbak@gmail.com>
2022-08-06 21:00:18 -0500

    Finishes PerfectNumber Challenge.

---
+++ PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,17 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number){
+        if (number < 1)
+            return false;
+
+        int sumDivisors = 0;
+
+        for (int i = 1 ; i < number ; i++){
+            if (number % i == 0){
+                sumDivisors += i;
+            }
+        }
+
+        return number == sumDivisors;
+    }
+}

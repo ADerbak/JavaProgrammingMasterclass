19323a6fdb82a2c042655c7d438184c6e71af0c7
Andrew <andrewderbak@gmail.com>
2022-08-06 20:12:42 -0500

    Completes LastDigitChecker Challenge

---
+++ LastDigitChecker/src/LastDigitChecker.java
@@ -0,0 +1,20 @@
+public class LastDigitChecker {
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        if (!isValid(num1) || !isValid(num2) || !isValid(num3))
+            return false;
+
+        int num1Digit = num1 % 10;
+        int num2Digit = num2 % 10;
+        int num3Digit = num3 % 10;
+
+        return (num1Digit == num2Digit || num1Digit == num3Digit || num2Digit == num3Digit);
+
+    }
+
+    public static boolean isValid(int num){
+        return (num >= 10 && num <= 1000 );
+
+    }
+}
+

b8808e2edf4143c30f9e7d14c72990081412c9d1
Andrew <andrewderbak@gmail.com>
2022-08-06 20:38:07 -0500

    Finishes GreatestCommonDivisor Challenge

---
+++ GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -0,0 +1,18 @@
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second){
+        if (first < 10 || second < 10)
+            return -1;
+
+        int divisor = 0;
+        int min = Math.min(first, second);
+
+        for (int i = 1; i <= min; i++){
+            if (first % i == 0 & second % i == 0 & i > divisor) {
+                divisor = i;
+            }
+
+        }
+        return divisor;
+    }
+}

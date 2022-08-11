4c6ed01ed6133293d065e549e6fdbe0020640c5d
Andrew <andrewderbak@gmail.com>
2022-08-10 20:23:41 -0500

    Completes MinAndMaxInput Challenge.

---
+++ MinAndMaxInputChallenge/src/MinAndMaxInput.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class MinAndMaxInput {
+
+    public static void getMinAndMax() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int min = 0, max = 0;
+
+        while (true) {
+
+
+            System.out.println("Enter a number: ");
+
+            boolean valid = scanner.hasNextInt();
+            if (valid) {
+                int num = scanner.nextInt();
+                scanner.nextLine();
+                if (min == 0 || num < min)
+                    min = num;
+                if (num > max)
+                    max = num;
+            }else{
+                    System.out.println("Invalid Value");
+                    break;
+                }
+
+
+            }
+
+        System.out.println("Min: "+ min);
+        System.out.println("Max: "+ max);
+    }
+}

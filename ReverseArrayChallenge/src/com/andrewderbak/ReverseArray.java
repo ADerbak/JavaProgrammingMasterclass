f591244a56a1543b9ade78c3d253891d423a7082
Andrew <andrewderbak@gmail.com>
2022-11-09 17:01:32 -0600

    Completes ReverseArray challenge

---
+++ ReverseArrayChallenge/src/com/andrewderbak/ReverseArray.java
@@ -0,0 +1,24 @@
+package com.andrewderbak;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        System.out.println(Arrays.toString(array));
+        reverse(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void reverse(int[] array){
+        int pointer = array.length-1;
+        for (int i = 0; i < (pointer+1)/2; i++){
+                int temp = array[pointer];
+                array[pointer] = array[i];
+                array[i] = temp;
+                pointer--;
+            }
+    }
+}

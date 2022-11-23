de52c5e5a21ac6421f9d2b4aa12085f68f937fbd
Andrew <andrewderbak@gmail.com>
2022-11-23 14:27:55 -0600

    Implements addInOrder to main method.

--- LinkedListDemo/src/com/andrewderbak/Demo.java
+++ LinkedListDemo/src/com/andrewderbak/Demo.java
@@ -8,23 +8,38 @@ public class Demo {
 
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<String>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
-
+        addInOrder(placesToVisit,"Sydney");
+        addInOrder(placesToVisit,"Melbourne");
+        addInOrder(placesToVisit,"Brisbane");
+        addInOrder(placesToVisit,"Perth");
+        addInOrder(placesToVisit,"Canberra");
+        addInOrder(placesToVisit,"Adelaide");
+        addInOrder(placesToVisit,"Darwin");
         printList(placesToVisit);
 
-        placesToVisit.add(1, "Alice Springs");
-
+        addInOrder(placesToVisit, "Alice Springs");
+        addInOrder(placesToVisit, "Darwin");
         printList(placesToVisit);
 
-        placesToVisit.remove(4); // can also remove by string value!
+//        placesToVisit.add("Sydney");
+//        placesToVisit.add("Melbourne");
+//        placesToVisit.add("Brisbane");
+//        placesToVisit.add("Perth");
+//        placesToVisit.add("Canberra");
+//        placesToVisit.add("Adelaide");
+//        placesToVisit.add("Darwin");
 
-        printList(placesToVisit);
+//        printList(placesToVisit);
+//
+//        placesToVisit.add(1, "Alice Springs");
+//
+//        printList(placesToVisit);
+//
+//        placesToVisit.remove(4); // can also remove by string value!
+//
+//        printList(placesToVisit);
+
+
 
     }
 

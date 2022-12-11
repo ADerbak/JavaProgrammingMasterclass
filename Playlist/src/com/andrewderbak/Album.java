1fb5335ee12b8acf9a59d44cb87af1327900ba18
Andrew <andrewderbak@gmail.com>
2022-12-10 20:06:39 -0600

    Creates fields and methods with TODOs for playlist project.

--- Playlist/src/com/andrewderbak/Album.java
+++ Playlist/src/com/andrewderbak/Album.java
@@ -1,4 +1,30 @@
 package com.andrewderbak;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public static boolean addSong(String title, double duration){
+        // TODO: return true if song was added successfully
+    }
+
+    public static Song findSong(String title){
+        // TODO: return Song if it exists, null otherwise
+
+    }
+
+    public static boolean addToPlayList(int trackNumber, LinkedList<Song> playlist){
+        // TODO: Returns true if it exists and it was added successfully using the track number
+        //  or false otherwise.
+    }
 }
--- Playlist/src/com/andrewderbak/Song.java
+++ Playlist/src/com/andrewderbak/Song.java
@@ -1,4 +1,22 @@
 package com.andrewderbak;
 
 public class Song {
+
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        String output = this.title + ": " + this.duration;
+        return super.toString();
+    }
 }
